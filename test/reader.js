333778e1c7bd5161f19b963fac7d3f20d18af94a
Ludwig Magnusson <ludwig@mediatool.com>
2017-12-10 14:18:14 +0100

    Make Reader fantasy-land compatible
    Instead of `map`, `chain` etc Reader now exposes `fantasy-land/map`,
    `fantasy-land/chain` etc.

--- test/reader.js
+++ test/reader.js
@@ -1,6 +1,8 @@
 import { assert } from 'chai'
 import { Reader, runReader, ask } from '../lib/reader'
 import ArgumentError from '../lib/argument-error'
+import { map, chain, ap, of } from '@theludd/fantasy-functions'
+import { pipe } from 'yafu'
 
 const { equal, throws } = assert
 
@@ -9,11 +11,11 @@ describe('reader', () => {
     return x + 1
   }
   function addEnv (n) {
-    return ask.map((env) => n + env)
+    return map((env) => n + env, ask)
   }
-  const r1 = Reader.of(1)
-  const incReader = Reader.of(inc)
-  const chainInc = (x) => Reader.of(x + 1)
+  const r1 = of(Reader, 1)
+  const incReader = of(Reader, inc)
+  const chainInc = (x) => of(Reader, x + 1)
 
   it('should work', () => {
     equal(runReader(null, r1), 1)
@@ -21,14 +23,14 @@ describe('reader', () => {
 
   describe('#map', () => {
     it('should work', () => {
-      const r2 = r1.map(inc)
+      const r2 = map(inc, r1)
       const result = runReader(null, r2)
       equal(result, 2)
     })
 
     it('should throw if map does not receive a function', () => {
       function errorFn () {
-        runReader(null, r1.map(1))
+        runReader(null, map(1, r1))
       }
       return throws(errorFn, ArgumentError)
     })
@@ -36,21 +38,21 @@ describe('reader', () => {
 
   describe('#chain', () => {
     it('should work', () => {
-      const r2 = r1.chain(chainInc)
+      const r2 = chain(chainInc, r1)
       const result = runReader(null, r2)
       equal(result, 2)
     })
 
     it('should warn if the chain fn does not return a reader', () => {
       function errorFn () {
-        runReader(null, r1.chain(inc))
+        runReader(null, chain(inc, r1))
       }
       return throws(errorFn, ArgumentError)
     })
 
     it('should warn if chain does not receive a function', () => {
       function errorFn () {
-        runReader(null, r1.chain(2))
+        runReader(null, chain(2, r1))
       }
       return throws(errorFn, ArgumentError)
     })
@@ -58,21 +60,21 @@ describe('reader', () => {
 
   describe('#ap', () => {
     it('should work', () => {
-      const r2 = r1.ap(incReader)
+      const r2 = ap(incReader, r1)
       const result = runReader(null, r2)
       equal(result, 2)
     })
 
     it('should warn if ap does not receive a Reader', () => {
       function errorFn () {
-        runReader(null, r1.ap(inc))
+        runReader(null, ap(inc, r1))
       }
       return throws(errorFn, ArgumentError)
     })
 
     it('should warn if ap receives a Reader not containing a function', () => {
       function errorFn () {
-        runReader(null, r1.ap(r1))
+        runReader(null, ap(r1, r1))
       }
       return throws(errorFn, ArgumentError, 'Reader#ap expected to receive Reader of function but got: Reader of 1')
     })
@@ -80,9 +82,9 @@ describe('reader', () => {
 
   describe('ask', () => {
     it('should work', () => {
-      const result = runReader(2, r1.chain((n) => {
-        return ask.map((env) => n + env)
-      }))
+      const result = runReader(2, chain((n) => {
+        return map((env) => n + env, ask)
+      }, r1))
       equal(result, 3)
     })
   })
@@ -93,19 +95,22 @@ describe('reader', () => {
     })
 
     it('should work for mapped readers', () => {
-      equal(r1.map(inc).toString(), 'Reader[2]')
+      equal(map(inc, r1).toString(), 'Reader[2]')
     })
   })
 
   it('should handle several transformations', () => {
-    const result = runReader(2, r1.map(inc)
-      .ap(incReader)
-      .chain(chainInc)
-      .map(inc)
-      .chain(chainInc)
-      .chain(addEnv)
-      .map(inc)
-      .ap(incReader))
+    const pipeLine = pipe([
+      map(inc),
+      ap(incReader),
+      chain(chainInc),
+      map(inc),
+      chain(chainInc),
+      chain(addEnv),
+      map(inc),
+      ap(incReader)
+    ])
+    const result = runReader(2, pipeLine(r1))
     equal(result, 10)
   })
 })
