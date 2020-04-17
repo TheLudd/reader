b33bb4f5d12e1fde9872354f1826110449a45715
Ludwig Magnusson <ludwig@mediatool.com>
2020-04-17 08:42:46 +0200

    chore: upgrde fantasy-functions
    The error checking has been removed since it is now a part of
    fantasy-functions.

--- test/reader.js
+++ test/reader.js
@@ -4,9 +4,8 @@ import {
 } from '@theludd/fantasy-functions'
 import { pipe } from 'yafu'
 import { Reader, runReader, ask } from '../lib/reader'
-import ArgumentError from '../lib/argument-error'
 
-const { equal, throws } = assert
+const { equal } = assert
 
 describe('reader', () => {
   function inc (x) {
@@ -29,13 +28,6 @@ describe('reader', () => {
       const result = runReader(null, r2)
       equal(result, 2)
     })
-
-    it('should throw if map does not receive a function', () => {
-      function errorFn () {
-        runReader(null, map(1, r1))
-      }
-      return throws(errorFn, ArgumentError)
-    })
   })
 
   describe('#chain', () => {
@@ -44,20 +36,6 @@ describe('reader', () => {
       const result = runReader(null, r2)
       equal(result, 2)
     })
-
-    it('should warn if the chain fn does not return a reader', () => {
-      function errorFn () {
-        runReader(null, chain(inc, r1))
-      }
-      return throws(errorFn, ArgumentError)
-    })
-
-    it('should warn if chain does not receive a function', () => {
-      function errorFn () {
-        runReader(null, chain(2, r1))
-      }
-      return throws(errorFn, ArgumentError)
-    })
   })
 
   describe('#ap', () => {
@@ -65,20 +43,6 @@ describe('reader', () => {
       const r2 = ap(incReader, r1)
       const result = runReader(null, r2)
       equal(result, 2)
-    })
-
-    it('should warn if ap does not receive a Reader', () => {
-      function errorFn () {
-        runReader(null, ap(inc, r1))
-      }
-      return throws(errorFn, ArgumentError)
-    })
-
-    it('should warn if ap receives a Reader not containing a function', () => {
-      function errorFn () {
-        runReader(null, ap(r1, r1))
-      }
-      return throws(errorFn, ArgumentError, 'Reader#ap expected to receive Reader of function but got: Reader of 1')
     })
   })
 
